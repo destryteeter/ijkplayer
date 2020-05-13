6512ef1a235a805700adf7016e8b500df77fa607
Destry Teeter <destry.teeter@gmail.com>
2020-05-13 16:03:55 -0700

    > Custom Modifications > > Add RTSP Playback support > Fix buffering issues causing RTSP stream to stop suddenly > Include RTSP demo url > Update bundle and development team settings > Fix FFMPEG build script

--- ios/IJKMediaDemo/IJKMediaDemo/IJKDemoSampleViewController.m
+++ ios/IJKMediaDemo/IJKMediaDemo/IJKDemoSampleViewController.m
@@ -32,9 +32,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.title = @"M3U8";
+    self.title = @"M3U8 / RTPS";
 
     NSMutableArray *sampleList = [[NSMutableArray alloc] init];
+    
+    [sampleList addObject:@[@"Wowza Demo",
+                            @"rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"]];
 
     [sampleList addObject:@[@"las url",
     @"{\"version\":\"1.0.0\",\"adaptationSet\":[{\"duration\":1000,\"id\":1,\"representation\":[{\"id\":1,\"codec\":\"avc1.64001e,mp4a.40.5\",\"url\":\"http://las-tech.org.cn/kwai/las-test_ld500d.flv\",\"backupUrl\":[],\"host\":\"las-tech.org.cn\",\"maxBitrate\":700,\"width\":640,\"height\":360,\"frameRate\":25,\"qualityType\":\"SMOOTH\",\"qualityTypeName\":\"流畅\",\"hidden\":false,\"disabledFromAdaptive\":false,\"defaultSelected\":false},{\"id\":2,\"codec\":\"avc1.64001f,mp4a.40.5\",\"url\":\"http://las-tech.org.cn/kwai/las-test_sd1000d.flv\",\"backupUrl\":[],\"host\":\"las-tech.org.cn\",\"maxBitrate\":1300,\"width\":960,\"height\":540,\"frameRate\":25,\"qualityType\":\"STANDARD\",\"qualityTypeName\":\"标清\",\"hidden\":false,\"disabledFromAdaptive\":false,\"defaultSelected\":false},{\"id\":3,\"codec\":\"avc1.64001f,mp4a.40.5\",\"url\":\"http://las-tech.org.cn/kwai/las-test.flv\",\"backupUrl\":[],\"host\":\"las-tech.org.cn\",\"maxBitrate\":2300,\"width\":1280,\"height\":720,\"frameRate\":30,\"qualityType\":\"HIGH\",\"qualityTypeName\":\"高清\",\"hidden\":false,\"disabledFromAdaptive\":false,\"defaultSelected\":true}]}]}"]]; 
